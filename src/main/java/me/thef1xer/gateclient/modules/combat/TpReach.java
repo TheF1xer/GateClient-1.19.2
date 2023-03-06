aa3f2a4b125d74255c2afc67e75a9934265ddaf1
TheF1xer <66278651+TheF1xer@users.noreply.github.com>
2023-03-06 00:09:21 -0300

    Improved raytracing

--- src/main/java/me/thef1xer/gateclient/modules/combat/TpReach.java
+++ src/main/java/me/thef1xer/gateclient/modules/combat/TpReach.java
@@ -2,11 +2,16 @@ package me.thef1xer.gateclient.modules.combat;
 
 import me.thef1xer.gateclient.modules.Module;
 import me.thef1xer.gateclient.utils.MathUtil;
+import me.thef1xer.gateclient.utils.RayTraceHit;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 import org.lwjgl.glfw.GLFW;
 
 public class TpReach extends Module {
@@ -16,37 +21,46 @@ public class TpReach extends Module {
 
     public void spam() {
         final ClientPlayerEntity player = MinecraftClient.getInstance().player;
+        final ClientWorld world = MinecraftClient.getInstance().world;
         double[] lookVec = MathUtil.getDirection(player.getYaw(), player.getPitch());
 
         Vec3d startVec = player.getEyePos();
         Vec3d endVec = startVec.add(lookVec[0] * 10, lookVec[1] * 10, lookVec[2] * 10);
-        Entity target = selectTargetEntity(startVec, endVec);
+        RayTraceHit.EntityHit entityHit = selectTargetEntity(startVec, endVec);
 
-        if (target == null) return;
+        if (entityHit.entity() == null) return;
 
-        System.out.println(target.getName().getString());
+        BlockHitResult blockHitResult = world.raycast(new RaycastContext(startVec, entityHit.hitPos(), RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, player));
+        if (blockHitResult != null) {
+            if (blockHitResult.getType() != HitResult.Type.MISS) return;
+        }
+
+        System.out.println(entityHit.entity().getName().getString());
     }
 
-    private Entity selectTargetEntity(Vec3d start, Vec3d end) {
+    private RayTraceHit.EntityHit selectTargetEntity(Vec3d start, Vec3d end) {
         final ClientPlayerEntity player = MinecraftClient.getInstance().player;
 
         Entity target = null;
+        Vec3d hitPos = null;
 
         // Select closest entity that can be the player is looking at
         for (Entity entity : MinecraftClient.getInstance().world.getEntities()) {
             if (entity == player) continue;
 
-            if (rayTraceHit(start, end, entity.getBoundingBox())) {
-                if (target == null || player.squaredDistanceTo(target) < player.squaredDistanceTo(entity)) {
-                    target = entity;
-                }
+            Vec3d newHitVec = rayTraceHit(start, end, entity.getBoundingBox());
+            if (newHitVec == null) continue;
+
+            if (target == null || player.squaredDistanceTo(target) < player.squaredDistanceTo(entity)) {
+                target = entity;
+                hitPos = newHitVec;
             }
         }
 
-        return target;
+        return new RayTraceHit.EntityHit(target, hitPos);
     }
 
-    private boolean rayTraceHit(Vec3d rayStart, Vec3d rayEnd, Box aabb) {
+    private Vec3d rayTraceHit(Vec3d rayStart, Vec3d rayEnd, Box aabb) {
         /*
         Algorithm based in linear algebra to determine if AABB gets hit by ray
 
@@ -65,17 +79,15 @@ public class TpReach extends Module {
 
         double[] lambdaInterval = {0, 1};
 
-        if (updateLambdaIntervalOneDimension(lambdaInterval, rayStart.x, rayEnd.x, aabb.minX, aabb.maxX)) return false;
-        if (updateLambdaIntervalOneDimension(lambdaInterval, rayStart.y, rayEnd.y, aabb.minY, aabb.maxY)) return false;
-        if (updateLambdaIntervalOneDimension(lambdaInterval, rayStart.z, rayEnd.z, aabb.minZ, aabb.maxZ)) return false;
+        if (updateLambdaIntervalOneDimension(lambdaInterval, rayStart.x, rayEnd.x, aabb.minX, aabb.maxX)) return null;
+        if (updateLambdaIntervalOneDimension(lambdaInterval, rayStart.y, rayEnd.y, aabb.minY, aabb.maxY)) return null;
+        if (updateLambdaIntervalOneDimension(lambdaInterval, rayStart.z, rayEnd.z, aabb.minZ, aabb.maxZ)) return null;
 
         if (lambdaInterval[0] <= lambdaInterval[1]) {
-            return true;
+            return rayStart.add((rayEnd.subtract(rayStart)).multiply(lambdaInterval[0]));
         }
 
-        System.out.println(lambdaInterval[0] + " " + lambdaInterval[1] + " ");
-        System.out.println(rayStart + " " + rayEnd + " " + aabb);
-        return false;
+        return null;
     }
 
     private boolean updateLambdaIntervalOneDimension(double[] currentInterval, double rayStart, double rayEnd, double boxMin, double boxMax) {
